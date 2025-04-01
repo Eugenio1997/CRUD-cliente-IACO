393a8191dbdec023d9b5bee33d3f645f1ab01da7
Eugenio Lopes Fernandes Lima <eugeniolopesfernandeslima1997@outlook.com>
2025-04-01 16:21:44 -0300

    adicionando forms e logica de cadastrado de cliente

---
+++ CRUD-cliente-IACO/Enums/GenerosEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CRUD_cliente_IACO.Enums
+{
+    public enum GenerosEnum
+    {
+        Homem = 0,
+        Mulher = 1,
+        PrefiroNaoIdentificar = 2,
+    }
+}
